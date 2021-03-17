4f3b76c9507178e789320e92b8191233506370fe
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-03-16 23:39:26 -0500

    added session and user login capability"

--- server.js
+++ server.js
@@ -9,15 +9,31 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
+app.use(session(sess));
 app.use(routes);
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
