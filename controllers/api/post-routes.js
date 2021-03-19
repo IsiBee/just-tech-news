bda4ea4ac2b92ebccc21477223a98133f28fc184
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-03-18 23:55:54 -0500

    added edit post capability

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -78,7 +79,7 @@ router.get('/:id', (req, res) => {
 });
 
 // Add a post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
@@ -92,7 +93,7 @@ router.post('/', (req, res) => {
 });
 
 // voting on a Post
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     if (req.session) {
         Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
             .then(updatedPostData => res.json(updatedPostData))
@@ -105,7 +106,7 @@ router.put('/upvote', (req, res) => {
 
 
 //update title of a post
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -130,7 +131,7 @@ router.put('/:id', (req, res) => {
 });
 
 // remove post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
